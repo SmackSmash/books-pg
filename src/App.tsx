1e7d45bd16252862bac44aa451caa5ca1d9f1c7a
Dan Moffat <danmoffat@gmail.com>
2025-07-10 23:29:12 +0100

    Added react-query and fetching books from postgres

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import type { FC } from 'react';
 import BookCreate from './components/BookCreate';
+import { useQuery } from '@tanstack/react-query';
 
 const App: FC = () => {
+  const { isFetching, error, data } = useQuery({
+    queryKey: ['books'],
+    queryFn: async () => {
+      const response = await fetch('http://localhost:5000/');
+      return await response.json();
+    }
+  });
+
   return (
     <div className='m-2 rounded bg-slate-700 p-4'>
       <h1 className='mb-2 border-b-2 border-b-slate-600 pb-2 text-2xl font-extrabold'>
