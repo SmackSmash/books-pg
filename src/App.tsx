6af2be3668313842434fdd87de191f52e5657eda
Dan Moffat <danmoffat@gmail.com>
2025-07-10 21:59:33 +0100

    A little styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import type { FC } from 'react';
 
 const App: FC = () => {
-  return <div>App works!</div>;
+  return (
+    <div className='m-2 rounded bg-slate-700 p-4'>
+      <h1 className='rounded bg-amber-700 p-2 text-2xl font-extrabold'>Reading List</h1>
+    </div>
+  );
 };
 
 export default App;
