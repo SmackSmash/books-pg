84b612f07eb9c4c0cef80eb4df329d9d1434b269
Dan Moffat <danmoffat@gmail.com>
2025-07-10 22:13:10 +0100

    Some more styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import type { FC } from 'react';
+import BookCreate from './components/BookCreate';
 
 const App: FC = () => {
   return (
     <div className='m-2 rounded bg-slate-700 p-4'>
-      <h1 className='rounded bg-amber-700 p-2 text-2xl font-extrabold'>Reading List</h1>
+      <h1 className='mb-2 border-b-2 border-b-slate-600 pb-2 text-2xl font-extrabold'>
+        Reading List
+      </h1>
+      <BookCreate />
     </div>
   );
 };
--- src/components/BookCreate.tsx
+++ src/components/BookCreate.tsx
@@ -1,7 +1,26 @@
 import type { FC } from 'react';
 
 const BookCreate: FC = () => {
-  return <div>BookCreate</div>;
+  return (
+    <div>
+      <h2 className='mb-4 text-xl font-bold'>Add a Book</h2>
+      <form className='flex flex-col'>
+        <label htmlFor='title' className='mb-2'>
+          Title
+        </label>
+        <div className='flex'>
+          <input
+            type='text'
+            id='title'
+            className='grow basis-1 bg-slate-100 p-2 text-slate-950 outline-none'
+          />
+          <button type='submit' className='cursor-pointer bg-amber-300 px-4 py-2 text-amber-950'>
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default BookCreate;
