4c4e28df4b89fb16f82abb4f92856105fabdcafe
Dan Moffat <danmoffat@gmail.com>
2025-07-11 01:41:42 +0100

    Added book image

--- src/components/BookShow.tsx
+++ src/components/BookShow.tsx
@@ -1,11 +1,13 @@
 import type { FC } from 'react';
 import type { Book } from '../types';
+import BookImage from '../assets/Closed_Book_Icon.svg';
 
 const BookShow: FC<{ book: Book }> = ({ book: { title, author } }) => {
   return (
-    <div className='basis-1/2 bg-slate-400'>
-      <h2>{title}</h2>
-      <h3>{author}</h3>
+    <div className='border-box basis-1/2 p-4'>
+      <img src={BookImage} alt='book' />
+      <h2 className='mt-2 text-center font-bold'>{title}</h2>
+      <h3 className='text-center'>{author}</h3>
     </div>
   );
 };
