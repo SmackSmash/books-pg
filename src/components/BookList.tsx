ea8e1fa8aba7f8fd50979a8a1c53799dfc2cf102
Dan Moffat <danmoffat@gmail.com>
2025-07-12 16:29:55 +0100

    A little styling

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -4,7 +4,7 @@ import BookShow from './BookShow';
 
 const BookList: FC<{ books: [Book] }> = ({ books }) => {
   return (
-    <div className='mb-2 flex flex-wrap rounded bg-slate-600 p-4'>
+    <div className='mb-2 grid grid-cols-2 gap-4 rounded bg-linear-to-br from-purple-500 to-pink-500 p-4'>
       {books.map(book => (
         <BookShow key={book.id} book={book} />
       ))}
--- src/components/BookShow.tsx
+++ src/components/BookShow.tsx
@@ -4,7 +4,7 @@ import BookImage from '../assets/Closed_Book_Icon.svg';
 
 const BookShow: FC<{ book: Book }> = ({ book: { title, author } }) => {
   return (
-    <div className='border-box basis-1/2 p-4'>
+    <div className='rounded bg-slate-100/50 p-4 text-slate-900'>
       <img src={BookImage} alt='book' />
       <h2 className='mt-2 text-center font-bold'>{title}</h2>
       <h3 className='text-center'>{author}</h3>
